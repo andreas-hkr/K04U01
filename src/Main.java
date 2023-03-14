e67f98d817177480d59a151552a2419945aff04f
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-14 19:05:17 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // Läs in hur många minuter per månad användaren ringer
@@ -5,6 +7,16 @@ public class Main {
         //   Normal  --> Mellan 33 och 66 (inklusive) minuter
         //   Plus    --> Mer än 66 minuter
 
+        Scanner input = new Scanner(System.in);
+        System.out.print("Hur många minuter uppskattar du att du kommer ringa per månad? ");
+        int minutes = input.nextInt();
 
+        if (minutes <= 33) {
+            System.out.println("Välj Kontant");
+        } else if (minutes <= 66) {
+            System.out.println("Välj Normal");
+        } else {
+            System.out.println("Välj Plus");
+        }
     }
 }
