bcac4239de4e01349646a2c81181df3b1ecc2af4
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-14 18:54:31 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Läs in hur många minuter per månad användaren ringer
+        //   Kontant --> 33 minuter eller mindre
+        //   Normal  --> Mellan 33 och 66 (inklusive) minuter
+        //   Plus    --> Mer än 66 minuter
+
+
+    }
+}
